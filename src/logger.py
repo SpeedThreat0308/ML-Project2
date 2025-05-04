aec90e7f0b17974c5b4669a1ce0ee9380367ef87
SpeedThreat0308 <thamizamuzhthu@gmail.com>
2025-05-04 15:10:12 +0530

    Logging and Exception done

--- src/logger.py
+++ src/logger.py
@@ -11,9 +11,6 @@ LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[-%(asctime)s-]-%(lineno)d-%(name)s-%(levelname)s-%(message)s",
-    level=logging.ERROR
+    level=logging.INFO
 )
 
-
-if __name__=="__main__":
-    logging.info("Logging has started")
