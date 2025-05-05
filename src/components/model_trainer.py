8863e27176c3c7dd4b02ca3358ef06f01f3c92e6
SpeedThreat0308 <thamizamuzhthu@gmail.com>
2025-05-05 19:16:35 +0530

    HyperParameter Tuning Done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,7 +46,38 @@ class ModelTrainer:
                 "AdaBoostRegressor": AdaBoostRegressor(),
             }
 
-            model_report:dict= evaluate_model(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test,models=models)
+            params={
+                "Decision Tree": {
+                    'criterion': ["squared_error","friedman_mse","absolute_error","poisson"]
+                },
+                "Random Forest Regressor": {
+                    'n_estimators': [8,16,32,64,126,256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1,.01,.05,.001],
+                    'subsample': [0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,126,256]
+                },
+                "Linear Regression":{},
+                "KNN Regressor":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,126,256]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01,0.05,0.1],
+                    'iterations': [30,50,100]
+                },
+                "AdaBoostRegressor": {
+                    'learning_rate': [.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,126,256]
+                }
+            }
+
+            model_report:dict= evaluate_model(X_train=X_train,X_test=X_test,y_train=y_train,y_test=y_test,models=models,param=params)
 
             best_model_score=max(sorted(model_report.values()))
 
